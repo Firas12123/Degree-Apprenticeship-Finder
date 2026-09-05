b974532437bc22195e4bddff16fe70c1d262b188
Firas12123 <firascosta35@gmail.com>
2026-09-05 21:31:29 +0100

    github workflow

--- main.py
+++ main.py
@@ -25,8 +25,9 @@ if jobs_dict:  # sync the database and call the dictionary of the different jobs
     connection.close()
 else:
     print("No jobs found for that title")
-    
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
@@ -34,9 +35,10 @@ def hello():
     jobs_list = get_jobs(cursor)
     connection.close()
     total_applied = sum(1 for job in jobs_list if job[4] == 1)
-    return render_template("DegreeApprenticeships.html", jobs = jobs_list, total_applied = total_applied)
+    return render_template("DegreeApprenticeships.html", jobs=jobs_list, total_applied=total_applied)
 
-@app.route("/toggle/<int:job_id>", methods = ["POST"])
+
+@app.route("/toggle/<int:job_id>", methods=["POST"])
 def toggle_applied(job_id):
     cursor, connection = db_sync()
     cursor.execute("SELECT applied FROM jobs WHERE jobId = ? AND hidden = 0", (job_id,))
@@ -47,11 +49,12 @@ def toggle_applied(job_id):
         new_status = 0 if current_status == 1 else 1
         cursor.execute("UPDATE jobs SET applied = ? WHERE jobId = ?", (new_status, job_id))
         connection.commit()
-        
+    
     connection.close()
     return "Updated Successfully", 200
 
-@app.route("/hide/<int:job_id>", methods= ["POST"])
+
+@app.route("/hide/<int:job_id>", methods=["POST"])
 def hide_job(job_id):
     cursor, connection = db_sync()
     cursor.execute("UPDATE jobs SET hidden = 1 WHERE jobId = ?", (job_id,))
@@ -61,4 +64,4 @@ def hide_job(job_id):
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
